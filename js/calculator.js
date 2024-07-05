3fcfcf54769b1a02fd88611114b9207a4adaa5bf
Phumulani <thusiphumulani1@gmail.com>
2024-07-05 12:52:14 +0200

    delete and clear screen

--- js/calculator.js
+++ js/calculator.js
@@ -8,7 +8,7 @@ let buttons = document.querySelectorAll('button');
 let directions = ['left','right','up','down'];
 let threeLetterFunctions = ['cos(','sin(','tan(','log('];
 let twoLetterFunctions   = ['ln('];
-let simpleSymbols = [...'0123456789.','x10'];
+let simpleSymbols = [...'0123456789.','*10**'];
 let operators     = ['+','*','/','-','='];
 let calcfunctions = ['del','clear','off','shift','mode','ans'];
 let nav = ['left','right','up','down'];
@@ -17,17 +17,33 @@ let nav = ['left','right','up','down'];
 buttons.forEach((button)=>{
     button.addEventListener("click",(event)=>{
         let input = event.target.value;
+        // console.log(input);
         if(directions.includes(input)){
             navigator.navigate(input);
-        }
-        if([...operators,...simpleSymbols].includes(input)){
+        }else if([...operators,...simpleSymbols].includes(input)){
             cursor.stopBlinking()
-            let screenContent = topDisplayScreen.innerText;
+            let screenContent = topDisplayScreen.innerHTML;
             let screenContentList = [...screenContent];
             screenContentList.splice(cursor.cursorIndex, 0, input);
             topDisplayScreen.innerHTML = screenContentList.join('');
             cursor.cursorIndex += 1;
             cursor.blink();
+        }else if(input==='del'){
+            if(cursor.cursorIndex>0){
+                cursor.stopBlinking();
+                let screenContent = topDisplayScreen.innerHTML;
+                let screenContentList = [...screenContent];
+                screenContentList.splice(cursor.cursorIndex-1, 1);            
+                cursor.cursorIndex -= 1;   
+                topDisplayScreen.innerHTML = screenContentList.join('');
+                cursor.blink();
+            }
+        }else if(input==='clear'){
+            cursor.stopBlinking()
+            topDisplayScreen.innerHTML = "";
+            cursor.cursorIndex = 0;  
+            cursor.blink();
         }
+
     });
 });
--- js/navigation.js
+++ js/navigation.js
@@ -6,19 +6,12 @@ export class Navigation{
     
     navigate(direction){
         let screenContent = this.screenDisplayer.innerHTML;
-        let screenContentList = [...screenContent];
-        let cursor = screenContentList[this.cursor.cursorIndex];
-        
         this.cursor.stopBlinking()
         if(direction==='left' && this.cursor.cursorIndex>0){
-            screenContentList[this.cursor.cursorIndex] = screenContentList[this.cursor.cursorIndex-1];
-            screenContentList[this.cursor.cursorIndex-1] = cursor;
             this.cursor.cursorIndex -= 1;
-
         }else if(direction === 'right'&& this.cursor.cursorIndex<screenContent.length-1){
             this.cursor.cursorIndex += 1;
         }
-        this.screenDisplayer.innerHTML = screenContentList.join("");
         this.cursor.blink();
     }
 }
